5e7bc26b2b4cff559577877e2c067e30cb43a318
CamargoKmila <camargo.kmila@gmail.com>
2021-03-08 03:23:27 -0300

    addition of total costs and maximum cost

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,3 +1,11 @@
 class Cost < ApplicationRecord
   belongs_to :deputy
+
+  def self.totalCost
+    self.sum(:vlrLiquido)
+  end
+
+  def self.maxCost
+    self.order('`vlrLiquido` desc').first
+  end
 end
