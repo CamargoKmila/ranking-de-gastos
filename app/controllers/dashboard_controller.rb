caf75c4250f017eaeca04b096029b204c9ae13c5
CamargoKmila <camargo.kmila@gmail.com>
2021-03-08 03:19:03 -0300

    addition of success and error messages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,11 @@
 class DashboardController < ApplicationController
   def index; end
+
+  def upload_csv
+    if Dashboard::UploadCsv.call(params[:data])
+      redirect_to root_path, alert: 'Dados importados com sucesso!!'
+    else
+      redirect_to root_path, alert: 'Erro ao importar dados!!'
+    end
+  end
 end
